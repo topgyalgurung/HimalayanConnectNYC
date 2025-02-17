dc6e05ce318f4c2c39fba3593c23a8dd7f8bef82
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-02-17 14:38:57 -0500

    syntax error fix for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,17 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 //   return NextResponse.redirect(new URL('/home', request.url))
-    const path = request.nextUrl.pathname
+  const path = request.nextUrl.pathname;
 
-    const isPublicPath = path === '/login' || path ==='/signup' || path === "/verifyemail" || path === "/passwordforgot" || path = "/passwordreset"
+  const isPublicPath = path === '/login' || path === '/signup' || path === "/verifyemail" || path === "/passwordforgot" || path = "/passwordreset";
     // access cookie 
     const token = request.cookies.get('token')?.value || ''
     // redirect authenticated user to '/'
     if (isPublicPath && token) {
-        return NextResponse.redirect( new URL ('/', request.nextUrl))
+      return NextResponse.redirect(new URL('/', request.nextUrl));
     }
     if (!isPublicPath && !token) {
-        return NextResponse.redirect( new URL ('/login', request.nextUrl))
+      return NextResponse.redirect(new URL('/login', request.nextUrl));
     }
 }
  
